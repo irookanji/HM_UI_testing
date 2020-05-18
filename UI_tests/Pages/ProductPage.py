1084f4feda03ee719fda21eae942c240d6033455
irookanji <murderer187@mail.ru>
2020-05-18 21:21:42 +0300

    Refactoring on ShoppingBagPage.py

--- UI_tests/Pages/ProductPage.py
+++ UI_tests/Pages/ProductPage.py
@@ -1,6 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 
 from .BaseApp import BasePage
 
