18f6655d7a9534be4cfd1052708dcaf5b7ef758f
irookanji <murderer187@mail.ru>
2020-06-30 20:51:07 +0300

    Refactor api tests example

--- UI_tests/Tests/test_api.py
+++ UI_tests/Tests/test_api.py
@@ -9,25 +9,25 @@ load = load_animal_facts()
 def test_cat_facts(api):
     cat_facts = api.facts.cat()
     assert cat_facts[0]["type"] == "cat" and cat_facts [1]["type"] == "cat"
-    assert cat_facts[0]["text"] != '' or None and cat_facts [1]["text"] != '' or None
+    assert cat_facts[0]["text"] != '' or None and cat_facts[1]["text"] != '' or None
 
 
 def test_dog_facts(api):
     dog_facts = api.facts.dog()
-    assert dog_facts[0]["type"] == "dog" and dog_facts [1]["type"] == "dog"
-    assert dog_facts[0]["text"] != '' or None and dog_facts [1]["text"] != '' or None
+    assert dog_facts[0]["type"] == "dog" and dog_facts[1]["type"] == "dog"
+    assert dog_facts[0]["text"] != '' or None and dog_facts[1]["text"] != '' or None
 
 
 def test_yandex_translate(api):
     translate = api.facts.yandex_translate()
     assert translate["text"] != '' or None
-    with open((os.path.abspath(r"..\animal_facts.json")),'w+',encoding="UTF-8") as f:
-        f.write(json.dumps(translate["text"],ensure_ascii=False))
+    with open((os.path.abspath(r"..\animal_facts.json")), 'w+', encoding="UTF-8") as f:
+        f.write(json.dumps(translate["text"], ensure_ascii=False))
 
 
 @pytest.mark.skip
-@pytest.mark.parametrize("animal_facts",load)
-def test_yandex_translate_multilang(api,animal_facts):
-    translate = api.facts.yandex_translate_multilang(lang='ru-en',text=animal_facts)
+@pytest.mark.parametrize("animal_facts", load)
+def test_yandex_translate_multilang(api, animal_facts):
+    translate = api.facts.yandex_translate_multilang(lang='ru-en', text=animal_facts)
     assert translate["text"] != '' or None
     print(translate)
--- UI_tests/config/config.py
+++ UI_tests/config/config.py
@@ -3,13 +3,13 @@ import os
 
 
 def load_config():
-    with open(os.path.abspath('../Wikipedia/qa_data.json'), encoding="UTF-8") as f:
+    with open(os.path.abspath('../UI_tests/animal_facts.json'), encoding="UTF-8") as f:
             json_data = json.load(f)
             qa_data_dict = json_data["test_data"]
     return qa_data_dict
 
 
 def load_animal_facts():
-    with open(os.path.abspath('../Wikipedia/animal_facts.json'), encoding="UTF-8") as f:
+    with open(os.path.abspath('../UI_tests/animal_facts.json'), encoding="UTF-8") as f:
         json_data = json.load(f)
     return json_data
